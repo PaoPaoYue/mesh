e0b38ae2765224fbdc5ed024193b7d4662236104
PaoPaoYue <846260131@qq.com>
2024-04-07 22:39:27 +1000

    add code generator

--- translate-application/src/main/java/com/github/paopaoyue/mesh/translate_application/service/ITranslateService.java
+++
@@ -1,7 +0,0 @@
-package com.github.paopaoyue.mesh.translate_application.service;
-
-import com.github.paopaoyue.mesh.translate_application.proto.Translate;
-
-public interface ITranslateService {
-    Translate.TranslateResponse translate(Translate.TranslateRequest request);
-}
