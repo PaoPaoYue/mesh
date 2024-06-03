9284ca3f9e94d839e0742dfe808847ee44e05da0
PaoPaoYue <846260131@qq.com>
2024-06-03 11:45:13 +1000

    add mock test

--- examples/demo/src/test/java/com/github/paopaoyue/DemoApplicationTests.java
+++ examples/demo/src/test/java/com/github/paopaoyue/DemoApplicationTests.java
@@ -1,13 +1,44 @@
 package com.github.paopaoyue;
 
+import com.github.paopaoyue.demo.DemoApplication;
+import com.github.paopaoyue.demo.api.IDemoCaller;
+import com.github.paopaoyue.demo.proto.DemoProto;
+import com.github.paopaoyue.demo.service.DemoService;
+import com.github.paopaoyue.demo.service.IDemoService;
+import com.github.paopaoyue.mesh.rpc.api.CallOption;
+import com.github.paopaoyue.mesh.rpc.service.MockRpcService;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.context.TestConfiguration;
 
-@SpringBootTest
+import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.when;
+
+@SpringBootTest(classes = DemoApplication.class)
 class DemoApplicationTests {
 
+    @Autowired
+    IDemoCaller demoCaller;
+
     @Test
-    void contextLoads() {
+    void MockTest() {
+        var response = demoCaller.echo(DemoProto.EchoRequest.newBuilder().setText("hello world").build(), new CallOption());
+        assertThat(response.getText()).isEqualTo("hello world");
     }
 
+    @TestConfiguration
+    public static class TestConfig {
+        @MockRpcService(serviceName = "dictionary-application")
+        public IDemoService mockDemoService() {
+            IDemoService service = mock(DemoService.class);
+            when(service.echo(any())).thenAnswer(invocation -> {
+                DemoProto.EchoRequest request = invocation.getArgument(0);
+                return DemoProto.EchoResponse.newBuilder().setText(request.getText()).build();
+            });
+            return service;
+        }
+    }
 }
