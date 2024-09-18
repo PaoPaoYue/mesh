1e1e862a6b4cadb03e153d49613dcaa6c0ffe2b7
PaoPaoYue <846260131@qq.com>
2024-09-18 22:25:56 +1000

    wait for health check

--- examples/demo-jdk8/src/main/java/io/github/paopaoyue/demo_jdk8/DemoApplication.java
+++ examples/demo-jdk8/src/main/java/io/github/paopaoyue/demo_jdk8/DemoApplication.java
@@ -4,6 +4,7 @@ import io.github.paopaoyue.demo_jdk8.api.IDemoCaller;
 import io.github.paopaoyue.demo_jdk8.proto.DemoProto;
 import io.github.paopaoyue.mesh.rpc.RpcAutoConfiguration;
 import io.github.paopaoyue.mesh.rpc.api.CallOption;
+import io.github.paopaoyue.mesh.rpc.util.RespBaseUtil;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -34,7 +35,11 @@ public class DemoApplication {
 
         public void run() {
             DemoProto.EchoResponse response = demoCaller.echo(DemoProto.EchoRequest.newBuilder().setText("hello world").build(), new CallOption());
-            logger.info("echo response: {}", response.getText());
+            if (RespBaseUtil.isOK(response.getBase())) {
+                logger.info("echo response: {}", response.getText());
+            } else {
+                logger.error("echo failed: {}", response.getBase().getMessage());
+            }
         }
     }
 
--- examples/demo/src/main/java/io/github/paopaoyue/demo/DemoApplication.java
+++ examples/demo/src/main/java/io/github/paopaoyue/demo/DemoApplication.java
@@ -4,6 +4,7 @@ import io.github.paopaoyue.demo.api.IDemoCaller;
 import io.github.paopaoyue.demo.proto.DemoProto;
 import io.github.paopaoyue.mesh.rpc.RpcAutoConfiguration;
 import io.github.paopaoyue.mesh.rpc.api.CallOption;
+import io.github.paopaoyue.mesh.rpc.util.RespBaseUtil;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -33,8 +34,19 @@ public class DemoApplication {
         private IDemoCaller demoCaller;
 
         public void run() {
+            if (RpcAutoConfiguration.getProp().isServerHealthCheckEnabled()) {
+                try {
+                    Thread.sleep(10000); // wait for server health check pass to add to the service registry
+                } catch (InterruptedException e) {
+                    throw new RuntimeException(e);
+                }
+            }
             var response = demoCaller.echo(DemoProto.EchoRequest.newBuilder().setText("hello world").build(), new CallOption());
-            logger.info("echo response: {}", response.getText());
+            if (RespBaseUtil.isOK(response.getBase())) {
+                logger.info("echo response: {}", response.getText());
+            } else {
+                logger.error("echo failed: {}", response.getBase().getMessage());
+            }
         }
     }
 
